84a52a92bfcf14a0b9287fc57932f63081210a6f
Christian Stein <sormuras@gmail.com>
2019-12-16 04:52:12 +0100

    Create module com.github.sormuras.bach.lwjgl

---
+++ src/com.github.sormuras.bach.lwjgl/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.github.sormuras.bach.lwjgl {
+  requires org.lwjgl;
+  requires org.lwjgl.natives;
+  requires org.lwjgl.glfw;
+  requires org.lwjgl.glfw.natives;
+  requires org.lwjgl.opengl;
+  requires org.lwjgl.opengl.natives;
+}
