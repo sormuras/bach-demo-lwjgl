ffba3480e7560a124239c18fb3296c4b3592b8e7
Christian Stein <sormuras@gmail.com>
2020-11-25 12:06:14 +0100

    Upgrade to latest project-info syntax

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -1,35 +1,16 @@
 import com.github.sormuras.bach.ProjectInfo;
 import com.github.sormuras.bach.ProjectInfo.Library;
 import com.github.sormuras.bach.ProjectInfo.Library.Link;
+import com.github.sormuras.bach.ProjectInfo.Library.Searcher;
 import com.github.sormuras.bach.ProjectInfo.Main;
 import com.github.sormuras.bach.ProjectInfo.Test;
 import com.github.sormuras.bach.ProjectInfo.Tweak;
+import com.github.sormuras.bach.module.ModuleSearcher.LWJGLSearcher;
 
 @ProjectInfo(
     name = "bach-lwjgl",
     version = "1",
-    library =
-        @Library(
-            links = {
-              @Link(
-                  module = "org.lwjgl",
-                  target = "org.lwjgl:lwjgl:3.2.3"),
-              @Link(
-                  module = "org.lwjgl.natives",
-                  target = "org.lwjgl:lwjgl:3.2.3:${LWJGL-NATIVES}"),
-              @Link(
-                  module = "org.lwjgl.glfw",
-                  target = "org.lwjgl:lwjgl-glfw:3.2.3"),
-              @Link(
-                  module = "org.lwjgl.glfw.natives",
-                  target = "org.lwjgl:lwjgl-glfw:3.2.3:${LWJGL-NATIVES}"),
-              @Link(
-                  module = "org.lwjgl.opengl",
-                  target = "org.lwjgl:lwjgl-opengl:3.2.3"),
-              @Link(
-                  module = "org.lwjgl.opengl.natives",
-                  target = "org.lwjgl:lwjgl-opengl:3.2.3:${LWJGL-NATIVES}")
-            }),
+    library = @Library(searchers = {@Searcher(with = LWJGLSearcher.class, version = "3.2.3")}),
     main =
         @Main(
             generateCustomRuntimeImage = true,
